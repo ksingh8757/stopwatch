98fc52cea5048a05e832fcccbac7a058a748540a
unknown <kavitakumari1911998@gmail.com>
2024-06-10 12:09:33 +0530

    v1

---
+++ script.js
@@ -0,0 +1,70 @@
+let isStop = true;
+let s = 0;
+let min = 0;
+let hr = 0;
+
+
+
+function start(){
+    
+    if(isStop == true)
+    {
+        isStop= false;
+        timer();
+    }
+
+}
+
+function timer(){
+    
+    if(isStop == false)
+    {
+        s = parseInt(s);
+        min = parseInt(min);
+        hr = parseInt(hr);
+
+        s++;
+        if(s==60){
+            s=0;
+            min++;
+        }
+
+        if(min==60){
+            min=0;
+            hr++;
+        }
+
+        if(s<10){
+            s = "0" + s;
+        }
+
+        if(min<10){
+            min = "0" + min;
+        }
+
+        if(hr<10){
+            hr = "0" + hr;
+        }
+
+
+        stopwatch.innerHTML = hr + " : " + min + " : " + s;
+        
+        setTimeout("timer()",1000);
+    }
+}
+
+
+
+function stop(){
+    isStop = true;
+}
+
+
+function reset(){
+    isStop=true;
+    s=0;
+    min=0;
+    hr=0;
+
+    stopwatch.innerHTML = "00 : 00 : 00";
+}
